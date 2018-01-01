c7e57a9e76163ef03cd82653f41ec8fe24ebb4fb
vedantv <vedant1966@gmail.com>
2018-01-01 21:57:05 +0530

    SVM implementation

---
+++ SVM implementation.py
@@ -0,0 +1,89 @@
+import matplotlib.pyplot as plt
+from matplotlib import style
+import numpy as np
+
+style.use('ggplot')
+
+class Support_Vector_Machine:
+    def __init__(self,visualisation=True):
+        self.visualisation = visualisation
+        self.color = {1:'r',-1:'b'}
+        if self.visualisation:
+            self.fig = plt.figure()
+            self.ax = self.add.fig.add_subplot(1,1,1)
+
+    def fit(self,data):
+        self.data = data
+        #||w||:[w,b]
+        opt_dict = {}
+
+        transforms = [[1,1],[-1,1],[-1,-1],[1,-1]]
+
+        all_data = []
+
+        for yi in self.data:
+            for featureset in self.data[yi]:
+                for feature in featureset:
+                    all_data.append(feature)
+
+        self.max_feature_value = max(all_data)
+        self.min_feature_value = min(all_data)
+        all_data = None
+
+        step_sizes = [self.max_feature_value*0.1,
+                      self.max_feature_value*0.01,
+                      self.max_feature_value*0.001]
+
+        b_range_multiple = 5
+        b_multiple = 5
+        latest_optimum = self.max_feature_value*10
+
+        for step in step_sizes:
+            w = np.array([latest_optimum,latest_optimum])
+            optimized = False
+            while not optimized:
+                for b in np.arange(-1*(self.max_feature_value*b_range_multiple),
+                                   self.max_feature_value*b_range_multiple,
+                                   step*b_multiple):
+                    for transformation in transforms:
+                        w_t = w*transformation
+                        found_option = True
+
+                        for i in self.data:
+                            for xi in self.data[i]:
+                                yi = i
+                                if not yi*(np.dot(w_t,xi)+b) >= 1:
+                                    found_option = False
+                        if found_option:
+                            opt_dict[np.linalg.norm(w_t)] = [w_t,b]
+
+                if w[0]<0:
+                    optimized = True
+                    print('optimized a step')
+                else:
+                    w = w - step
+            norms = sorted(n for n in opt_dict)
+            opt_choice =  opt_dict[norms[0]]
+            self.w = opt_choice[0]
+            self.b = opt_choice[1]
+            latest_optimum = opt_choice[0][0] + step*2
+
+    def predict(self,features):
+        #sign of equation x.w+b
+        classification = np.sign(np.dot(np.array(features),self.w) + self.b)
+        if classification != 0 and self.visualisation:
+            self.ax.scatter(features[0],features[1],s=200,marker=*,c=self.color[classification])
+        return classification
+    #optional function for plottting of SVM data for better understanding
+    def visualize(self):
+        [[self.ax.scatter(x[0],x[1],s=100,marker=*,c=self.color[i])for xi in data_dict[i]]for i in data_dict]
+
+
+
+
+
+
+
+
+data_dict = {-1:np.array([[2,3],[4,5],[1,3],]),
+             1:np.array([[7,8],[9,8],[11,3],])}
